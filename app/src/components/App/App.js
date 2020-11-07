7cee108d2078b1aa71030aa7fdb06d99018ed73c
Jason Ogasian <jason@joyrun.com>
2020-11-06 21:21:56 -0800

    Snowflake. Just for fun

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,14 +1,23 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSnowflake } from "@fortawesome/free-solid-svg-icons";
 import LocalWeather from "components/LocalWeather/LocalWeather";
 import SkiTahoe from "components/SkiTahoe/SkiTahoe";
 
 import './App.scss';
 
+
+// Icons
+library.add(faSnowflake);
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Weather Findr
+        Weather Findr&nbsp;&nbsp;
+        <FontAwesomeIcon icon={faSnowflake} />
       </header>
 
       <Router>
